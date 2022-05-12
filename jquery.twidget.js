dae4c27511fd1939abcc029adf62611b2c70622c
Prince Dev <princedev432@gmail.com>
2022-05-13 00:03:19 +0530

    Update jquery.twidget.js

--- jquery.twidget.js
+++ jquery.twidget.js
@@ -28,7 +28,7 @@
             marker: 1111,
             type: 'avia_hotel',
             hide_logos: true,
-            open_in_new_tab: false,
+            open_in_new_tab: true,
             default_origin: '',
             default_destination: '',
 			lock_destination: false,
